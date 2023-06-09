5aafd25214e8a4ecfe61af33003888f0d54cdbc2
NixonNababan <105839395+NixonNababan@users.noreply.github.com>
2023-06-09 11:12:59 +0800

    Update response.go

--- Code_Competence/models/payload/response.go
+++ Code_Competence/models/payload/response.go
@@ -2,19 +2,6 @@ package payload
 
 import uuid "github.com/satori/go.uuid"
 
-type DashboardUser struct {
-	Adoption int `json:"adoption" form:"adoption"`
-	Donation int `json:"donation" form:"donation"`
-}
-
-type DashboardAdmin struct {
-	TotalUser         int `json:"total_user" form:"total_user"`
-	TotalAdoption     int `json:"total_adoption" form:"total_adoption"`
-	TotalDonation     int `json:"total_donation" form:"total_donation"`
-	TotalPetAvailable int `json:"total_pet_available" form:"total_pet_available"`
-	TotalPetAdopted   int `json:"total_pet_adopted" form:"total_pet_"`
-}
-
 type GetProfil struct {
 	Name     string `json:"name" form:"name"`
 	Username string `json:"username" form:"username"`
